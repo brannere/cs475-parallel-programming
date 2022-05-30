378cb4ad3e540669e8788c92e4836becc64945e1
Erick-Branner <brannere@oregonstate.edu>
2022-05-30 13:14:09 -0700

    new third run

--- project-6-opencl-array/dgx_submit.bash
+++ project-6-opencl-array/dgx_submit.bash
@@ -18,9 +18,9 @@ PROC3=""
 
 echo "OVERWRITE making headers for csv files"
 
-echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultsPerSecond" > dgx.first.csv
-echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultsPerSecond" > dgx.second.csv
-echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultiply-Adds Per Second" > dgx.third.csv
+#echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultsPerSecond" > dgx.first.csv
+#echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultsPerSecond" > dgx.second.csv
+echo "NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, GigaMultiply-Adds Per Second" > dgx.other.third.csv
 
 
 echo "running all three programs"
@@ -28,13 +28,13 @@ for gs in 1 512 1024 2048 4096 8192
 do
   for ls in 8 16 32 64 128 256 512
   do
-    g++ -w -o first first.cpp -DNMB=$gs -DLOCAL_SIZE=$ls /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
-    ./first >> dgx.first.csv
+    #g++ -w -o first first.cpp -DNMB=$gs -DLOCAL_SIZE=$ls /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+    #./first >> dgx.first.csv
 
-    g++ -w -o second second.cpp -DNMB=$gs -DLOCAL_SIZE=$ls /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
-    ./second >> dgx.second.csv
+    #g++ -w -o second second.cpp -DNMB=$gs -DLOCAL_SIZE=$ls /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+    #./second >> dgx.second.csv
 
     g++ -w -o third third.cpp -DNMB=$gs -DLOCAL_SIZE=$ls /usr/local/apps/cuda/10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
-    ./third >> dgx.third.csv &
+    ./third >> dgx.other.third.csv &
   done
 done
